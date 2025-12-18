d3422064651b079efd61bfd096d54e347b455962
lightbringher13 <lightbringher13@gmail.com>
2025-12-18 23:32:32 +0900

    chore(tests): update test package path to match new package naming

--- backend/notification-service/src/test/java/com/timeeconomy/notification/NotificationServiceApplicationTests.java
+++ backend/notification-service/src/test/java/com/timeeconomy/notification/NotificationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.timeeconomy.gateway_service;
+package com.timeeconomy.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class NotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
